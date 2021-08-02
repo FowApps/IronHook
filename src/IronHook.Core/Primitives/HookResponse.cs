4be13314f42487fbbd2cceb8cd4c2e8c398fe486
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-02 12:41:17 +0300

    ⛏ Create Hook Response

---
+++ src/IronHook.Core/Primitives/HookResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronHook.Core.Primitives
+{
+    public struct HookResponse
+    {
+        public Dictionary<string, string> Headers { get; set; }
+        public string Body { get; set; }
+        public int StatusCode { get; set; }
+        public bool IsExceeded { get; set; }
+        public string EventName { get; set; }
+
+        public string Url { get; set; }
+
+        public Guid RequestId { get; set; }
+
+        public string NotifyEmail { get; set; }
+    }
+}
