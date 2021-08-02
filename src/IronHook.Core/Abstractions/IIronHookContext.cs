f7014fce68f8166dd112ae13370cccea31ea49b0
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-02 15:12:38 +0300

    Create IIronHookContext

---
+++ src/IronHook.Core/Abstractions/IIronHookContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronHook.Core.Abstractions
+{
+    /// <summary>
+    /// Abstraction of Iron Hook Db Context
+    /// </summary>
+    public interface IIronHookContext
+    {
+        /// <summary>
+        /// Get Queryable Entity
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of Entity
+        /// </typeparam>
+        /// <returns>
+        /// Type of IQueryable
+        /// </returns>
+        IQueryable<T> Get<T>() where T : class;
+
+        /// <summary>
+        /// Insert Entity
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of Entity
+        /// </typeparam>
+        /// <param name="entity">
+        /// Entity
+        /// </param>
+        /// <returns>
+        /// Task
+        /// </returns>
+        Task InsertAsync<T>(T entity) where T : class;
+    }
+}
