f3b2cb7f91c50b163496b5768374d7b166495630
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-03 13:14:25 +0300

    ⛏ Add endpoints for IHookContext
    - Add Update entity endpoint
    - Add Delete entity endpoint

--- src/IronHook.Core/Abstractions/IIronHookContext.cs
+++ src/IronHook.Core/Abstractions/IIronHookContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IronHook.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -35,5 +36,33 @@ namespace IronHook.Core.Abstractions
         /// Task
         /// </returns>
         Task InsertAsync<T>(T entity) where T : class;
+
+        /// <summary>
+        /// Update Entity
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of Entity
+        /// </typeparam>
+        /// <param name="entity">
+        /// Entity
+        /// </param>
+        /// <returns>
+        /// Task
+        /// </returns>
+        Task UpdateAsync<T>(T entity) where T : class;
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of Entity
+        /// </typeparam>
+        /// <param name="entity">
+        /// Entity
+        /// </param>
+        /// <returns>
+        /// Task
+        /// </returns>
+        Task DeleteAsync<T>(T entity) where T : class;
     }
 }
