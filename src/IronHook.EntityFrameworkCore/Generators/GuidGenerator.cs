875b4517c0ecd106afd8b2d173812318f92ae2a4
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-03 11:35:10 +0300

    ⛏ Create Guid Generator
    This generator will uses when row insert set PK

---
+++ src/IronHook.EntityFrameworkCore/Generators/GuidGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronHook.EntityFrameworkCore.Generators
+{
+    /// <summary>
+    /// Guid value generator for primary key.
+    /// </summary>
+    public class GuidGenerator : ValueGenerator
+    {
+        public override bool GeneratesTemporaryValues => false;
+
+        protected override object NextValue(EntityEntry entry)
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
