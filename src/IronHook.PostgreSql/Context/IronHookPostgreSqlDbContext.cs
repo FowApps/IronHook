4730ab3f7454e7d1639bf9f5ba6beba04e54a55c
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-03 14:02:55 +0300

    ⛏ Add PostgreSql DbContext for IronHook

---
+++ src/IronHook.PostgreSql/Context/IronHookPostgreSqlDbContext.cs
@@ -0,0 +1,24 @@
+﻿using IronHook.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronHook.PostgreSql.Context
+{
+    /// <summary>
+    /// Iron Hook DbContext for PostgreSql
+    /// </summary>
+    public class IronHookPostgreSqlDbContext : IronHookCoreDbContext
+    {
+        public IronHookPostgreSqlDbContext()
+        {
+        }
+
+        public IronHookPostgreSqlDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
