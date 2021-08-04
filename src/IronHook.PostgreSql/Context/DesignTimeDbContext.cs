95e1c89f07f8bf3c7a922d48881a671d04da831f
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-04 11:27:16 +0300

    ⛏ Create DesignTimeDbcontext for PostgreSql provider

---
+++ src/IronHook.PostgreSql/Context/DesignTimeDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronHook.PostgreSql.Context
+{
+    public class DesignTimeDbContext : IDesignTimeDbContextFactory<IronHookPostgreSqlDbContext>
+    {
+        public IronHookPostgreSqlDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<IronHookPostgreSqlDbContext>();
+            optionsBuilder.UseNpgsql("xxxx");
+            return new IronHookPostgreSqlDbContext(optionsBuilder.Options);
+        }
+    }
+}
