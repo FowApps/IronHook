517ffc1c017639c0b6c4ca0dba3fce0e5a2b3faa
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-05 10:45:18 +0300

    ⛏ Implement Create Hook and Get Types endpoint for sample project

---
+++ samples/IronHook.Web/Controllers/HookController.cs
@@ -0,0 +1,80 @@
+﻿using IronHook.Core.Abstractions;
+using IronHook.Core.Entities;
+using IronHook.PostgreSql.Context;
+using IronHook.Web.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IronHook.Web.Controllers
+{
+    /// <summary>
+    /// Hook Endpoints
+    /// </summary>
+    [ApiController]
+    [Route("hooks")]
+    public class HookController : ControllerBase
+    {
+        private readonly IHookService<IronHookPostgreSqlDbContext> hookService;
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="hookService">
+        /// Hook Operation Service
+        /// </param>
+        public HookController(IHookService<IronHookPostgreSqlDbContext> hookService)
+        {
+            this.hookService = hookService;
+        }
+
+        /// <summary>
+        /// Add Hook
+        /// </summary>
+        /// <param name="model">
+        /// Hook Request Data Transfer Object
+        /// </param>
+        /// <returns>
+        /// Hook Entity
+        /// </returns>
+        [HttpPost(Name = "AddHook")]
+        [ProducesResponseType(typeof(Hook), 200)]
+        public async Task<IActionResult> AddHookAsync([FromBody] HookRequestDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var response = await hookService.AddAsync(new Hook
+            {
+                Name = model.Name,
+                TenantId = "1",
+                Key = model.Key
+            });
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Get Hook Types
+        /// </summary>
+        /// <returns>
+        /// Array of string
+        /// </returns>
+        [HttpGet("types", Name = "GetTypes")]
+        [ProducesResponseType(typeof(string[]), 200)]
+        public IActionResult GetTypes()
+        {
+            var result = GetFieldValuesFromType(typeof(EventNames));
+            return Ok(result.ToList());
+            static IEnumerable<string> GetFieldValuesFromType(Type type)
+            {
+                foreach (var field in type.GetProperties().Where(x => x.PropertyType == typeof(string)))
+                {
+                    yield return field.GetValue(null) as string;
+                }
+            }
+        }
+    }
+}
