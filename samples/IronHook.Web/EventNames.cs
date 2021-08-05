6cfb6724a50ad26f43fed92d2d7707699e214994
Furkan Güngör <furkan.dvlp@gmail.com>
2021-08-05 10:39:35 +0300

    ⛏ Create EventNames class
    This class will uses get hook types endpoint

---
+++ samples/IronHook.Web/EventNames.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IronHook.Web
+{
+    /// <summary>
+    /// All Events
+    /// </summary>
+    public static class EventNames
+    {
+        /// <summary>
+        /// All Event
+        /// </summary>
+        public static string ALL_EVENT { get => "*"; }
+
+        /// <summary>
+        /// Customer Created Event
+        /// </summary>
+        public static string CUSTOMER_CREATED { get => "customer.created"; }
+
+        /// <summary>
+        /// Customer Update Event
+        /// </summary>
+        public static string CUSTOMER_UPDATED { get => "customer.updated"; }
+
+        /// <summary>
+        /// Customer Delete Event
+        /// </summary>
+        public static string CUSTOMER_DELETED { get => "customer.deleted"; }
+
+        /// <summary>
+        /// Customer Read Event
+        /// </summary>
+        public static string CUSTOMER_READED { get => "customer.readed"; }
+    }
+}
