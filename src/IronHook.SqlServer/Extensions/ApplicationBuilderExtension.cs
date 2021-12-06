98a9527e057b7062045402db63b808ede82111e2
Furkan Güngör <furkan.dvlp@gmail.com>
2021-12-06 14:39:47 +0300

    🔨 Add service collection and application builder extension

---
+++ src/IronHook.SqlServer/Extensions/ApplicationBuilderExtension.cs
@@ -0,0 +1,33 @@
+﻿using IronHook.SqlServer.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IronHook.SqlServer.Extensions
+{
+    /// <summary>
+    /// This class includes Application Builder extensions for database migrations.
+    /// Implements database migration.
+    /// </summary>
+    public static class ApplicationBuilderExtension
+    {
+        /// <summary>
+        /// Database Migration for SqlServer
+        /// </summary>
+        /// <param name="app">
+        /// IApplicationBuilder
+        /// </param>
+        /// <returns>
+        /// Application Builder interface
+        /// </returns>
+        public static IApplicationBuilder UseIronHook(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<IronHookSqlServerDbContext>();
+                context.Database.Migrate();
+            }
+            return app;
+        }
+    }
+}
---
+++ src/IronHook.SqlServer/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,36 @@
+﻿using IronHook.Core.Abstractions;
+using IronHook.Core.Concrete;
+using IronHook.SqlServer.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace IronHook.SqlServer.Extensions
+{
+    /// <summary>
+    /// This class includes IServiceCollection extensions
+    /// </summary>
+    public static class ServiceCollectionExtension
+    {
+        /// <summary>
+        /// Initialize Iron Hook
+        /// </summary>
+        /// <param name="services">
+        /// IServiceCollection
+        /// </param>
+        /// <param name="options">
+        /// Action of DbContextOptionsBuilder
+        /// </param>
+        /// <returns>
+        /// IServiceCollection
+        /// </returns>
+        public static IServiceCollection AddIronHook(this IServiceCollection services, Action<DbContextOptionsBuilder> options)
+        {
+            services.AddDbContext<IronHookSqlServerDbContext>(options, ServiceLifetime.Transient, ServiceLifetime.Transient);
+            services.AddTransient<IIronHookContext>(sp => sp.GetService<IronHookSqlServerDbContext>());
+            services.AddTransient<IHookService<IronHookSqlServerDbContext>, DefaultHookService<IronHookSqlServerDbContext>>();
+            services.AddTransient<IHookOperator, HttpHookOperator>();
+            return services;
+        }
+    }
+}
