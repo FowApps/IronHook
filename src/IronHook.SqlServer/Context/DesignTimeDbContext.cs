cb563960d993afd101177a6f368080412f574bb5
Furkan Güngör <furkan.dvlp@gmail.com>
2021-12-06 13:35:08 +0300

    🔨 Add Design Time Db Context

---
+++ src/IronHook.SqlServer/Context/DesignTimeDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronHook.SqlServer.Context
+{
+    public class DesignTimeDbContext : IDesignTimeDbContextFactory<IronHookSqlServerDbContext>
+    {
+        public IronHookSqlServerDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<IronHookSqlServerDbContext>();
+            optionsBuilder.UseSqlServer("{YOUR_CONNECTION_STRING}");
+            return new IronHookSqlServerDbContext(optionsBuilder.Options);
+        }
+    }
+}
