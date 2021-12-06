dd95e6e75cc87a8d03aba705352788014f2901f3
Furkan Güngör <furkan.dvlp@gmail.com>
2021-12-06 13:34:55 +0300

    🔨 Add IronHook Sql Server Db Context

---
+++ src/IronHook.SqlServer/Context/IronHookSqlServerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IronHook.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronHook.SqlServer.Context
+{
+    public class IronHookSqlServerDbContext : IronHookCoreDbContext
+    {
+        public IronHookSqlServerDbContext()
+        {
+        }
+
+        public IronHookSqlServerDbContext(DbContextOptions<IronHookSqlServerDbContext> options) : base(options)
+        {
+        }
+    }
+}
