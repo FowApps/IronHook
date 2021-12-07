cb6b2ccd374ee20ea05c9083a481528cc22dc371
enisn <info@enisnecipoglu.com>
2021-12-08 01:20:50 +0300

    ✨ Add SqlServer project

---
+++ src/IronHook.EntityFrameworkCore.SqlServer/Extensions/DbContextBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace IronHook.EntityFrameworkCore.SqlServer.Extensions
+{
+    public static class DbContextBuilderExtensions
+    {
+        public static SqlServerDbContextOptionsBuilder UseIronHookSqlServerMigrations(this SqlServerDbContextOptionsBuilder builder)
+        {
+            builder.MigrationsAssembly(typeof(IronHookCoreDbContextFactory).Assembly.FullName);
+
+            return builder;
+        }
+    }
+}
---
+++ src/IronHook.EntityFrameworkCore.SqlServer/IronHookCoreDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using IronHook.EntityFrameworkCore.SqlServer.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace IronHook.EntityFrameworkCore.SqlServer
+{
+    public class IronHookCoreDbContextFactory : IDesignTimeDbContextFactory<IronHookCoreDbContext>
+    {
+        public IronHookCoreDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<IronHookCoreDbContext>();
+
+            optionsBuilder.UseSqlServer(opts => opts.UseIronHookSqlServerMigrations());
+
+            return new IronHookCoreDbContext(optionsBuilder.Options);
+        }
+    }
+}
